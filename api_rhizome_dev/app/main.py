cf4d10d239d5553d13d095ac773439c4bfdd01d1
Brian Li <brian@brianli.com>
2022-02-22 13:55:42 +0900

    Update main.py

--- api_rhizome_dev/app/main.py
+++ api_rhizome_dev/app/main.py
@@ -1,6 +1,8 @@
+from sys import prefix
 from api_rhizome_dev.app.routers import icx
 from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import RedirectResponse
 
 app = FastAPI()
 
@@ -21,6 +23,11 @@ app.add_middleware(
 app.include_router(icx.router, prefix="/api/v1")
 
 
-@app.get("/healthz/", status_code=status.HTTP_204_NO_CONTENT)
+@app.get("/", include_in_schema=False)
+async def index():
+    return RedirectResponse("/docs")
+
+
+@app.get("/healthz/", status_code=status.HTTP_204_NO_CONTENT, include_in_schema=False)
 async def health_check():
     return
